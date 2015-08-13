ce77ebe822db68ef220ca0691a020ba9179b205e
lwcM <ms0472904@gmail.com>
2015-08-14 02:35:00 +0800

    disjoint_set (path compreesion & union by rank)

--- disjoin_set.cpp
+++ disjoin_set.cpp
@@ -1,18 +1,17 @@
 /**
 by lwc
+Path Compression + Union by rank
 **/
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
 #define MAXN 10001
 
-int Group[MAXN], Group_num[MAXN];
+int Group[MAXN], Rank[MAXN];
 
 inline int find(int k)
 {
-	return Group[k] == k ? Group[k] : (Group[k] = find(Group[k]));
+	return Group[k] == k ? Group[k] : Group[k] = find(Group[k]);
 }
 
 inline void Union(int x, int y)
@@ -20,16 +19,20 @@ inline void Union(int x, int y)
 	int xx = find(x), yy = find(y);
 	if(xx == yy)
 		return;
-	if(sz[xx] > sz[yy])
-		swap(xx, yy);
-	Group[xx] = yy;
-	sz[xx] = 0;
-	sz[yy] += sz[xx];
+	if(Rank[xx] < Rank[yy])
+		Group[xx] = yy;
+	else if(Rank[xx] > Rank[yy])
+		Group[yy] = xx;
+	else
+		Rank[ Group[yy] = xx ]++;
 }
 
 int main()
 {
 	//initial
 	for (int i = 1; i <= 10001; ++i)
-		f[i] = i;
+	{
+		Group[i] = i;
+		Rank[i] = 0;
+	}
 }
